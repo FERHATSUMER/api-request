d4149f456c9b26f17fb28e21df21e7dfaf3c45e2
FERHATSUMER <sumerf39@gmail.com>
2023-05-16 13:30:48 +0300

    api-request

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import axios from 'axios';
 import './App.css';
 import Header from './components/header';
 import Search from './components/search';
 
 function App() {
+  
+const searchImages= async (term)=>{
+const response=await  axios.get('https://api.unsplash.com/search/photos',{
+  headers:{
+    Authorization:'Client-ID 2V90XiC31En_Bs_dsNV-xa7EizJ1R9iJNizThVFW-CI'
+  },
+  params:{
+    query:term
+  }
+})
+return response.data.results
+}
+
+const handleSubmit= (term)=>{
+  debugger;
+  console.log(term)
+  searchImages(term)
+}
   return (
     <div className="App">
     <Header />
-    <Search />
+    <Search  search={handleSubmit} />
   </div>
   );
 }
--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,13 +1,23 @@
 import { FaSearch } from 'react-icons/fa';
 import './search.css';
 
-const Search = () => {
+const Search = ({search}) => {
+
+  const handleFromSubmit= (event)=>{
+    event.preventDefault ()
+    debugger;
+      search("ferhat")
+  }
+
   return (
     <form className='form-container'>
       <label className='search-title'>What are you looking?</label>
       <div className='search-container'>
         <FaSearch className='search-icon' />
-        <input className='search-input' placeholder='write anything' />
+        <input
+        onSubmit={handleFromSubmit}
+        className='search-input' 
+        placeholder='write anything' />
       </div>
     </form>
   );
