bd95480eea2afcea1277ff1320aab5085057d7ab
FERHATSUMER <sumerf39@gmail.com>
2023-05-16 17:32:50 +0300

    api-request

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,31 +1,31 @@
-import { FaSearch } from 'react-icons/fa';
-import './search.css';
-import { useState } from 'react';
+import { FaSearch } from "react-icons/fa";
+import "./search.css";
+import { useState } from "react";
 
-const Search = ({search}) => {
-  const [value,setValue]=useState("ca")
-
-  const handleFromSubmit= (event)=>{
-    event.preventDefault ()
+const Search = ({ search }) => {
+  const [valueInput, setValue] = useState("");
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
     debugger;
-      search("ferhat")
-  }
+    search(valueInput);
+  };
 
- const handleChange = ()=>{
-  setValue(e.target.value) 
- }
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
 
   return (
-    <form className='form-container'>
-      <label className='search-title'>What are you looking?</label>
-      <div className='search-container'>
-        <FaSearch className='search-icon' />
+    <form className="form-container">
+      <label className="search-title">What are you looking?</label>
+      <div className="search-container">
+        <FaSearch className="search-icon" />
         <input
-        value={value}
-        onChange={handleChange}
-        onSubmit={handleFromSubmit}
-        className='search-input' 
-        placeholder='write anything' />
+          value={valueInput}
+          onChange={handleChange}
+          onSubmit={handleFormSubmit}
+          className="search-input"
+          placeholder="write anything"
+        />
       </div>
       <div>{setValue}</div>
     </form>
